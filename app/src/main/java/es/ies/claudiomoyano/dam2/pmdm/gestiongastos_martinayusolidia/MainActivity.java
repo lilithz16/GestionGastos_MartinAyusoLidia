e5f2c8c65b670ed6870ecad7c2095f894b8440f1
lilithz16 <lil.lithz1616@gmail.com>
2025-11-13 10:42:44 +0100

    :)

---
+++ app/src/main/java/es/ies/claudiomoyano/dam2/pmdm/gestiongastos_martinayusolidia/MainActivity.java
@@ -0,0 +1,17 @@
+package es.ies.claudiomoyano.dam2.pmdm.gestiongastos_martinayusolidia;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+    }
+}
